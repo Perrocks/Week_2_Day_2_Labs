5afeee16a6b2960ffd7a75b5934e46bde3c2d12c
Samuel Williams <samuel.williams.ed@gmail.com>
2022-11-29 13:51:57 +0000

    minor refactors

--- multiple_classes/pet_shop_start_point/classes/customer.py
+++ multiple_classes/pet_shop_start_point/classes/customer.py
@@ -19,4 +19,3 @@ class Customer():
         for pet in self.pets:
             total += pet.price
         return total
-        
--- multiple_classes/pet_shop_start_point/classes/pet_shop.py
+++ multiple_classes/pet_shop_start_point/classes/pet_shop.py
@@ -28,15 +28,12 @@ class PetShop:
                 return pet
 
     def sell_pet_to_customer(self, pet_name, customer_object):
+        #variable assignments
         pet_being_sold = self.find_pet_by_name(pet_name)
         pet_cost = pet_being_sold.price
-        #reduce customer cash
+        #functions
         customer_object.reduce_cash(pet_cost)
-        #add pet to customer
         customer_object.add_pet(customer_object)
-        #add cash to shop
         self.increase_total_cash(pet_cost)
-        #remove pet from shop
         self.remove_pet(pet_being_sold)
-        #increase pets sold count
         self.increase_pets_sold()
